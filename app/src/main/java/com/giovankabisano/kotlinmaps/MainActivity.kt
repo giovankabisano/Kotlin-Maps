aa82d59e87300f217801120594e804a95290f05b
Giovanka Bisano <gbisano@gmail.com>
2020-06-26 18:31:06 +0700

    Add MapsFragment.kt

--- app/src/main/java/com/giovankabisano/kotlinmaps/MainActivity.kt
+++ app/src/main/java/com/giovankabisano/kotlinmaps/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.giovankabisano.kotlinmaps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.main_nav_host_fragment)
+        val navGraph = navController.navInflater.inflate(R.navigation.main_graph)
+        navGraph.startDestination = R.id.mapsFragment
+        navController.graph = navGraph
     }
 }
